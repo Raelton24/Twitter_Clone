5767ea543e1a26fdcded66547da03747633f6f1d
Raelton24 <israeluyi7@gmail.com>
2025-03-08 22:35:00 +0100

    auth_controller_protectRoute_added

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,18 @@ import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes.js"
 import connectMongoDB from "./db/connectMongoDb.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000
 
-app.use("/api/auth", authRoutes);
+app.use(express.json());
+app.use(express.urlencoded({extended: true})); // to pass form data (urlencoded)
+app.use(cookieParser());
 
+app.use("/api/auth", authRoutes);
 
 app.listen(3000, () => {
     console.log(`Server running on port ${PORT}`)
